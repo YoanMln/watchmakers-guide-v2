d0769539ad14b3ecd3297d186f2d989f3b10c1df
YoanMln <yoan.moulin@hotmail.fr>
2026-06-02 15:02:36 +0200

    feat: add home page (index.vue)

---
+++ app/pages/index.vue
@@ -0,0 +1,39 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    "data-page": "home",
+  },
+});
+</script>
+
+<template>
+  <header>
+    <div class="header-container">
+      <div class="title-h1">
+        <h1>Watchmaker's Guide</h1>
+      </div>
+      <div class="intro-container">
+        <h2>
+          Explorez la précision et la passion de l’horlogerie à travers un
+          regard technique et authentique
+        </h2>
+      </div>
+      <div class="btn-container-home">
+        <NuxtLink to="/decouvrir">Découvrir l'horlogerie</NuxtLink>
+      </div>
+      <div class="nav-home">
+        <ul class="nav-home-list">
+          <li>
+            <NuxtLink to="/fonctionnement"> Fonctionnment</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/outils">Outils de l'horloger</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="techniques">Techniques</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
