b4873de97dfe7636edbff8e86f0d9b7218e353e0
YoanMln <yoan.moulin@hotmail.fr>
2026-06-02 14:50:02 +0200

    feat: add AppNav and AppFooter components

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <div class="footer__content">
+      <p class="footer__name">Watchmaker's Guide</p>
+      <p class="footer__text">&copy; 2026 - Tous droits réservés.</p>
+    </div>
+  </footer>
+</template>
