53763025c88779706ce8652927f7994ffab4d923
YoanMln <yoan.moulin@hotmail.fr>
2026-06-04 21:59:32 +0200

    style: improve timeline styling and add Google Fonts integration (#10)
    * style: improve timeline styling and add Google Fonts integration
    
    * fix: add preconnect to font for better loading performance
    
    * refactor: update color variable and timeline styling values

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,23 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/scss/main.scss"],
   modules: ["@nuxt/icon"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Alumni+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+      ],
+    },
+  },
 });
