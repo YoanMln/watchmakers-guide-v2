7a6d30c279c27a7c6b14e5441aca9963273b3ebb
YoanMln <yoan.moulin@hotmail.fr>
2026-07-08 10:00:14 +0200

    style: update PageHeader style and move background folder to public/images (#24)
    * style: update PageHeader style and move background folder to public/images
    
    * fix: remove v-if=image in the template
    
    * style: fix separator (set display:block) and page-header

--- app/components/PageHeader.vue
+++ app/components/PageHeader.vue
@@ -1,7 +1,7 @@
 <script setup>
 defineProps({
   title: { type: String, required: true },
-  subtitle: { type: String, required: false },
+  subtitle: { type: String, required: true },
 });
 </script>
 
@@ -9,10 +9,14 @@ defineProps({
   <div class="page-header">
     <div class="page-header__content">
       <h1 class="page-header__title">{{ title }}</h1>
+      <span class="page-header__separator"></span>
       <h2 v-if="subtitle" class="page-header__sub">{{ subtitle }}</h2>
     </div>
-    <div v-if="$slots.actions" class="page-header__actions">
-      <slot name="actions" />
+
+    <div class="page-header__actions-container">
+      <div v-if="$slots.actions" class="page-header__actions">
+        <slot name="actions" />
+      </div>
     </div>
   </div>
 </template>
