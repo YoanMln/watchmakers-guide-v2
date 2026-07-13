0523658e5afc747531a61ebab3e422ad3827bc60
YoanMln <yoan.moulin@hotmail.fr>
2026-07-13 11:49:58 +0200

    feat: create AccordionItems component and implement it on techniques page (#25)
    * feat: create AccordionItems component and implement it on techniques page
    
    * fix: improve :key detail for more stability and add aria-expanded on button
    
    * fix: correct AccordionItems chevron selector

---
+++ app/components/AccordionItems.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  title: { type: String, required: true }, // Titre de l'accordéon
+  details: { type: Array, required: true }, // Tableau JSON
+  image: { type: String, required: true },
+  isOpen: { type: Boolean, default: false }, // Indique si l'accordéon est ouvert
+});
+
+const emit = defineEmits(["toggle"]); // Event émis quand on clique sur l'en-tête
+</script>
+
+<template>
+  <div class="accordion-items" :class="{ 'accordion-items--open': isOpen }">
+    <button
+      class="accordion-items__header"
+      type="button"
+      :aria-expanded="isOpen"
+      @click="emit('toggle')"
+    >
+      <h2 class="accordion-items__title">{{ title }}</h2>
+      <Icon class="accordion-items__chevron" name="mdi:chevron-down" />
+    </button>
+    <Transition name="accordion">
+      <div v-if="isOpen" class="accordion-items__content">
+        <ul class="accordion-items__details">
+          <li v-for="(detail, index) in details" :key="`${title}-${index}`">
+            <Icon class="accordion-items__icons-list" name="mdi:gear-outline" />
+            {{ detail }}
+          </li>
+        </ul>
+        <img :src="image" :alt="title" class="accordion-items__image" />
+      </div>
+    </Transition>
+  </div>
+</template>
